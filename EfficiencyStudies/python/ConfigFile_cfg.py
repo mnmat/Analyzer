29e2b9e18dbc9f6d4c283717152b528b0fa9a522
Mark <m.matthewman@gmail.com>
2022-08-05 15:23:37 +0200

    Refactor ConfigFile

--- EfficiencyStudies/python/ConfigFile_cfg.py
+++ EfficiencyStudies/python/ConfigFile_cfg.py
@@ -13,7 +13,7 @@ process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 
 # Specify Eta
 
-eta = "Eta_29" # Eta_16, Eta_17, Eta_29, Eta_17_27
+eta = "Eta_16" # Eta_16, Eta_17, Eta_29, Eta_17_27
 
 """
 
@@ -26,16 +26,26 @@ process.source = cms.Source("PoolSource",
 outfile_  = 'file:file:/afs/cern.ch/user/m/mmatthew/CMSSW_12_3_0_pre5/src/hgcal_private/production/'+eta+'/singleel_flatEGun_hgcalCenter/step3/ttree_singleel_e100GeV_nopu.root'
 
 """
+
 process.source = cms.Source("PoolSource",
                                 # replace 'myfile.root' with the source file you want to use
                                 fileNames = cms.untracked.vstring(
-            'file:/afs/cern.ch/user/m/mmatthew/CMSSW_12_3_0_pre5/src/hgcal_private/production/'+eta+'/singlemuon_flatEGun_hgcalCenter/step3/step3_singlemuon_e100GeV_nopu.root'
+            'file:/eos/home-m/mmatthew/Data/Analyzer/EfficiencyStudies/' + eta + '/singlemuon_flatEGun_hgcalCenter/step3/step3_singlemuon_e100GeV_nopu.root'
                 )
                             )
-outfile_  = 'file:file:/afs/cern.ch/user/m/mmatthew/CMSSW_12_3_0_pre5/src/hgcal_private/production/'+eta+'/singlemuon_flatEGun_hgcalCenter/step3/ttree_singlemuon_e100GeV_nopu.root'
+outfile_  = 'file:file:/eos/home-m/mmatthew/Data/Analyzer/EfficiencyStudies/'+eta+'/singlemuon_flatEGun_hgcalCenter/step3/ttree_singlemuon_e100GeV_nopu.root'
 
-#"""
 
+"""
+process.source = cms.Source("PoolSource",
+                                # replace 'myfile.root' with the source file you want to use
+                                fileNames = cms.untracked.vstring(
+            'file:/afs/cern.ch/user/m/mmatthew/CMSSW_12_3_0_pre5/src/hgcal_private/production/singleel/singleel_flatEGun_hgcalCenter/step3/step3_singleel_e100GeV_nopu.root'
+                )
+                            )
+outfile_  = 'file:file:/afs/cern.ch/user/m/mmatthew/CMSSW_12_3_0_pre5/src/hgcal_private/production/singleel/singleel_flatEGun_hgcalCenter/step3/ttree_singleel_e100GeV_nopu.root'
+
+"""
 process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string(outfile_),
                                    closeFileFast = cms.untracked.bool(True)
@@ -45,7 +55,7 @@ process.TFileService = cms.Service("TFileService",
 process.demo = cms.EDAnalyzer('EfficiencyStudies',
    tracks = cms.untracked.InputTag('generalTracks'),
    caloParticles = cms.InputTag("mix", "MergedCaloTruth"),
-   Tracksters = cms.InputTag("ticlSimTracksters","fromCPs","RECO"),
+   Tracksters = cms.InputTag("ticlTrackstersMerge","","RECO"),
    hgcalRecHitsEE = cms.InputTag("HGCalRecHit", "HGCEERecHits"),
    hgcalRecHitsFH = cms.InputTag("HGCalRecHit", "HGCHEFRecHits"),
    hgcalRecHitsBH = cms.InputTag("HGCalRecHit", "HGCHEBRecHits"),
